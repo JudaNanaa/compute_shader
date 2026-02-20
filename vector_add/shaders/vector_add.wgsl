f3628cf5db7ab1466177bf9dbcf1dfd0591d3b62
JudaNanaa <moussaadamou898@gmail.com>
2026-02-20 15:21:38 +0100

    do introduction and vector_sum compute shader

---
+++ vector_add/shaders/vector_add.wgsl
@@ -0,0 +1,27 @@
+// A read-only storage buffer that stores and array of unsigned 32bit integers
+@group(0) @binding(0) var<storage, read> first_input: array<u32>;
+// A read-only storage buffer that stores and array of unsigned 32bit integers
+@group(0) @binding(1) var<storage, read> second_input: array<u32>;
+// This storage buffer can be read from and written to
+@group(0) @binding(2) var<storage, read_write> output: array<u32>;
+
+// Tells wgpu that this function is a valid compute pipeline entry_point
+@compute
+// Specifies the "dimension" of this work group
+@workgroup_size(256)
+fn main(
+    // global_invocation_id specifies our position in the invocation grid
+    @builtin(global_invocation_id) global_invocation_id: vec3<u32>
+) {
+    let index = global_invocation_id.x;
+    let total = arrayLength(&first_input);
+
+    // workgroup_size may not be a multiple of the array size so
+    // we need to exit out a thread that would index out of bounds.
+    if (index >= total) {
+        return;
+    }
+
+    // a simple copy operation
+    output[index] = first_input[index] + second_input[index];
+}
---
+++ vector_add/src/main.rs
@@ -0,0 +1,157 @@
+use flume::bounded;
+use rand::RngExt;
+use wgpu::util::DeviceExt;
+
+fn create_random_vec(size: u32) -> Vec<u32> {
+    let mut dest = Vec::with_capacity(size as usize);
+    let mut rng = rand::rng();
+
+    for _ in 0..size {
+        let rng_value = rng.random_range(0..size);
+        dest.push(rng_value);
+    }
+    return dest;
+}
+
+async fn add_two_vec(first_vec: &Vec<u32>, second_vec: &Vec<u32>) -> anyhow::Result<Vec<u32>> {
+    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::PRIMARY,
+        ..Default::default()
+    });
+
+    let adapter = instance.request_adapter(&Default::default()).await.unwrap();
+
+    let (device, queue) = adapter.request_device(&Default::default()).await.unwrap();
+
+    let shader = device.create_shader_module(wgpu::include_wgsl!("../shaders/vector_add.wgsl"));
+
+    let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: Some("Introduction Compute Pipeline"),
+        layout: None,
+        module: &shader,
+        entry_point: None,
+        compilation_options: Default::default(),
+        cache: Default::default(),
+    });
+
+    let first_input_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("first input"),
+        contents: bytemuck::cast_slice(&first_vec),
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::STORAGE,
+    });
+
+    let second_input_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("second input"),
+        contents: bytemuck::cast_slice(&second_vec),
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::STORAGE,
+    });
+
+    let output_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("output"),
+        size: first_input_buffer.size(),
+        usage: wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::STORAGE,
+        mapped_at_creation: false,
+    });
+
+    let temp_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("temp"),
+        size: first_input_buffer.size(),
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
+        mapped_at_creation: false,
+    });
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &pipeline.get_bind_group_layout(0),
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: first_input_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: second_input_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: output_buffer.as_entire_binding(),
+            },
+        ],
+    });
+
+    let mut encoder = device.create_command_encoder(&Default::default());
+
+    {
+        // We specified 64 threads per workgroup in the shader, so we need to compute how many
+        // workgroups we need to dispatch.
+        let num_dispatches = first_vec.len().div_ceil(64) as u32;
+
+        let mut pass = encoder.begin_compute_pass(&Default::default());
+        pass.set_pipeline(&pipeline);
+        pass.set_bind_group(0, &bind_group, &[]);
+        pass.dispatch_workgroups(num_dispatches, 1, 1);
+    }
+
+    encoder.copy_buffer_to_buffer(&output_buffer, 0, &temp_buffer, 0, output_buffer.size());
+
+    queue.submit([encoder.finish()]);
+
+    let mut sum_vec: Option<Vec<u32>> = None;
+
+    {
+        // The mapping process is async, so we'll need to create a channel to get
+        // the success flag for our mapping
+        let (tx, rx) = bounded(1);
+
+        // We send the success or failure of our mapping via a callback
+        temp_buffer.map_async(wgpu::MapMode::Read, .., move |result| {
+            tx.send(result).unwrap()
+        });
+
+        // The callback we submitted to map async will only get called after the
+        // device is polled or the queue submitted
+        device.poll(wgpu::PollType::wait_indefinitely())?;
+
+        // We check if the mapping was successful here
+        rx.recv_async().await??;
+
+        // We then get the bytes that were stored in the buffer
+        let output_data = temp_buffer.get_mapped_range(..);
+
+        sum_vec = Some(bytemuck::cast_slice(&output_data).to_vec());
+    }
+
+    // We need to unmap the buffer to be able to use it again
+    temp_buffer.unmap();
+
+    println!("Success!");
+
+    return Ok(sum_vec.unwrap());
+}
+
+const SIZE: u32 = 1000000;
+
+fn main() {
+    let first_vec = create_random_vec(SIZE);
+    let second_vec = create_random_vec(SIZE);
+
+    env_logger::init();
+
+    let sum_vec = pollster::block_on(add_two_vec(&first_vec, &second_vec));
+
+    if let Ok(tab) = sum_vec {
+        // Somme CPU
+        let cpu_sum: Vec<u32> = first_vec
+            .iter()
+            .zip(second_vec.iter())
+            .map(|(a, b)| a + b)
+            .collect();
+
+        // Vérification
+        if cpu_sum == tab {
+            println!("✅ GPU result is correct!");
+        } else {
+            println!("❌ Mismatch detected!");
+        }
+    }
+}
